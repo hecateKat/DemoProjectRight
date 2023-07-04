0d3b395e48efa7b33b1027739a8e2a167c1187e1
Seweryn Drazek <seweeryn.drazek@lhsystems.com>
2023-07-04 14:45:33 +0200

    Initial commit

---
+++ src/main/java/com/demoprojectright/DemoProjectRightApplication.java
@@ -0,0 +1,13 @@
+package com.demoprojectright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectRightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectRightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoprojectright/DemoProjectRightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoprojectright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectRightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
